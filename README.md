5fc223711f177707e2dd8dbf79653c15b933605a
nayeemmehedi <boyneel79@gmail.com>
2021-02-11 19:43:18 +0600

    Who am I

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # API_changSentanceToRefresh
+# random_myName
--- api.js
+++ api.js
@@ -1,13 +1,20 @@
-fetch('http://www.boredapi.com/api/activity/')
+fetch('https://randomuser.me/api')
   .then(response => response.json())
-  .then(json => namebd (json))
+  .then(json => {
 
-  let namebd = data =>{
-      
-      let a = data.activity
+    let json_result =json.results[0]
 
-      let b = document.getElementById("bored_bd")
-      b.innerText = a
+
+    let  final_work = json_result.name
+    
+    full_name = `${final_work.title} ${final_work.first} ${final_work.last}`
+
+     let html_id = document.getElementById("name")
+     let value = html_id.innerText = full_name
+
+  })
+
+ 
   
-  }
+  
 
